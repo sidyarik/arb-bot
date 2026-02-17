bcd3b32bd78571dd48069708846e05c246547107
Yaroslav Sidorenko <sidyarik@gmail.com>
2026-02-17 17:50:18 +0000

    debug service

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
 
+print("=== DEBUG START ===")
+print("SERVICE NAME:", os.getenv("RAILWAY_SERVICE_NAME"))
+print("ENV NAME:", os.getenv("RAILWAY_ENVIRONMENT_NAME"))
+print("ALL ENV KEYS:", list(os.environ.keys()))
+print("=== DEBUG END ===")
+
+raise RuntimeError("STOP DEBUG")
+
 def get_required_env(name):
     value = os.environ.get(name)
     if value is None:
@@ -16,3 +24,4 @@ BINANCE_SECRET = get_required_env("BINANCE_SECRET")
 
 FUNDING_THRESHOLD = float(os.getenv("FUNDING_THRESHOLD", -0.0005))
 SCAN_INTERVAL_SEC = int(os.getenv("SCAN_INTERVAL_SEC", 120))
+
