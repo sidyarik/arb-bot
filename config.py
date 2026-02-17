d3110dd3d9ab05c7e422f88c5ec9c05b888f4c1e
Yaroslav Sidorenko <sidyarik@gmail.com>
2026-02-17 17:32:05 +0000

    debug env

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import os
 
+print("ALL ENV VARS:", dict(os.environ))
+
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = int(os.getenv("TELEGRAM_CHAT_ID"))
+print("TOKEN:", TELEGRAM_TOKEN)
+
+chat_id = os.getenv("TELEGRAM_CHAT_ID")
+print("CHAT_ID RAW:", chat_id)
+
+if chat_id is None:
+    raise ValueError("TELEGRAM_CHAT_ID NOT FOUND")
+
+TELEGRAM_CHAT_ID = int(chat_id)
 
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
 BINANCE_SECRET = os.getenv("BINANCE_SECRET")
