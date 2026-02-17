7e824cd15e2b17580ba34c7170144563a373bcba
Yaroslav Sidorenko <sidyarik@gmail.com>
2026-02-17 18:24:04 +0000

    switch to dotenv

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-print("=== DEBUG START ===")
-print("SERVICE NAME:", os.getenv("RAILWAY_SERVICE_NAME"))
-print("ENV NAME:", os.getenv("RAILWAY_ENVIRONMENT_NAME"))
-print("ALL ENV KEYS:", list(os.environ.keys()))
+load_dotenv()
 
 def get_required_env(name):
     value = os.environ.get(name)
     if value is None:
-        print(f"❌ ENV VARIABLE NOT FOUND: {name}")
-        print("Current ENV keys:", list(os.environ.keys()))
         raise RuntimeError(f"{name} not set")
     return value
 
@@ -19,5 +15,5 @@ TELEGRAM_CHAT_ID = int(get_required_env("TELEGRAM_CHAT_ID"))
 BINANCE_API_KEY = get_required_env("BINANCE_API_KEY")
 BINANCE_SECRET = get_required_env("BINANCE_SECRET")
 
-FUNDING_THRESHOLD = float(os.getenv("FUNDING_THRESHOLD", -0.0005))
-SCAN_INTERVAL_SEC = int(os.getenv("SCAN_INTERVAL_SEC", 120))
+FUNDING_THRESHOLD = float(os.getenv("FUNDING_THRESHOLD", -0.005))
+SCAN_INTERVAL_SEC = int(os.getenv("SCAN_INTERVAL_SEC", 10))
